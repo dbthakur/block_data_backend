44279950d7e56050b6f39e1d7ed18e986f97d8ad
omprakash-dwivedi28 <ompraksh.dwivedi28@gmail.com>
2025-03-26 17:27:12 +0530

    Modify the json output for inspected by

--- services/fetchservice/inspected_by.service.js
+++ services/fetchservice/inspected_by.service.js
@@ -5,7 +5,7 @@ export const getInspectedByService = async () => {
   
       const query = `SELECT * FROM  inspected_by`;
       const [rows] = await db.query(query);
-      return rows;
+      return {status:"ok",rows};
     } catch(error) {
       console.error("Error in getInspectedByService:", error);
       throw error;
