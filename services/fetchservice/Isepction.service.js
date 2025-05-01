706011601108a1e33c4177c2cc2e50d0777df082
dbthakur <deenbandhuthakur022@gmail.com>
2025-05-01 10:46:25 +0530

    modified api to show max date of inspection insted of max ins_date

--- services/fetchservice/Isepction.service.js
+++ services/fetchservice/Isepction.service.js
@@ -5,7 +5,7 @@ export const getTinsepctionService = async () => {
   try {
     // console.log("Came in service");
 
-    const query = `SELECT * FROM t_inspection_note ORDER BY ins_date DESC`;
+    const query = `SELECT * FROM t_inspection_note ORDER BY date_of_inspection DESC`;
     const [rows] = await db.query(query); // No need for an empty array
 
     // console.log("Rows in service:", rows);
@@ -24,7 +24,7 @@ export const getMaxInsDateService = async () => {
 
 
 
-    const query = `SELECT id,max(ins_date) as max_ins_date,stations FROM t_inspection_note group by stations`;
+    const query = `SELECT id,max(date_of_inspection) as max_date_of_inspection,stations FROM t_inspection_note group by stations`;
     const [rows] = await db.query(query); // No need for an empty array
 
     // console.log("Rows in service:", rows);
