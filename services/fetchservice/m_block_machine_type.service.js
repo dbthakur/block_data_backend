349c618b891932fb9f26a47c29193b6b456d6aa1
OM PRAKASH DWIVEDI <omprakash.dwivedi28@gmail.com>
2025-06-20 11:52:23 +0530

    Fix the new grouping column in getMachineType

--- services/fetchservice/m_block_machine_type.service.js
+++ services/fetchservice/m_block_machine_type.service.js
@@ -2,9 +2,9 @@ import {db} from "../../db/dbConnect.js";
 import { groupByLevels } from "../../utils/groupByLevels.js";
 export const getMachineType = async () => {
     try {
-        const query = `SELECT TYPE,ASSET,ASSET_DISCRIPTION FROM m_block_machine_type`;
+        const query = `SELECT DEPARTMENT,TYPE,ASSET,ASSET_DISCRIPTION FROM m_block_machine_type`;
         const [result] = await db.query(query);
-        const groupResult = groupByLevels(result, ['TYPE','ASSET']);
+        const groupResult = groupByLevels(result, ['DEPARTMENT','TYPE','ASSET']);
         return groupResult;
     } catch (error) {
         console.error("Error fetching machine types:", error);
